fdc75dc6334663a597febb61393b94793b40ed8d
Pranit <pranit.raje@hashicorp.com>
2025-05-27 11:03:48 +0530

    Comment out default values for EC2 instance variables in variables.tf

--- variables.tf
+++ variables.tf
@@ -4,19 +4,19 @@
 variable "instance_name" {
   description = "Name for the EC2 instance"
   type        = string
-  default     = "my-ec2-instance"
+  # default     = "my-ec2-instance"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  # default     = "t3.micro"
 }
 
 variable "instance_ami" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0953476d60561c955" # Example for us-east-1
+  # default     = "ami-0953476d60561c955" # Example for us-east-1
 }
 
 # variable "db_name" {
